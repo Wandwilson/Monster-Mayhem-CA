84b378458707811eba8fd149d53207cb8d333f60
wandyalmeida <2021230@student.cct.ie>
2024-06-02 14:41:24 +0100

    add mosnter.js to start the comunication form client to server

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const WebSocket = require('ws');
 const app = express();
 const server = require('http').createServer(app);
 const wss = new WebSocket.Server({ server });
+const games = {};
+const players = {};
 
 
 app.set('view engine', 'ejs');
@@ -16,6 +18,23 @@ app.use(express.static(path.join(__dirname, 'static')));
 
 const indexRouter = require('./routes/index');
 app.use('/', indexRouter);
+
+function getGameState(gameId) {
+    return games[gameId] || null;
+}
+
+function updateGameState(gameId, state) {
+    games[gameId] = state;
+}
+
+wss.on('connection', (ws) => {
+    ws.on('message', (message) => {
+        const data = JSON.parse(message);
+        handleGameMessage(ws, data);
+    });
+
+    ws.send(JSON.stringify({ message: 'Welcome to Monster Mayhem!' }));
+});
 
 
 server.listen(8080, () => {
